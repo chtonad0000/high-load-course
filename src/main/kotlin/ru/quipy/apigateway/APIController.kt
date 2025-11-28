6809ce022e6517aba87f63ccfa477ce7b78c39ef
danchub04 <d_chub04@mail.ru>
2025-11-28 13:16:54 +0300

    ready case 5

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.quipy.common.utils.TokenBucketRateLimiter
+import ru.quipy.common.utils.LeakingBucketRateLimiter
+import ru.quipy.common.utils.CompositeRateLimiter
 import ru.quipy.orders.repository.OrderRepository
 import ru.quipy.payments.logic.OrderPayer
 import java.util.*
@@ -22,10 +24,18 @@ class APIController {
 
     @Autowired
     private lateinit var orderPayer: OrderPayer
-
-    //case 1: private val paymentRateLimiter = TokenBucketRateLimiter(11, 13, 1, TimeUnit.SECONDS)
-    //case 2: private val paymentRateLimiter = TokenBucketRateLimiter(11, 132, 1, TimeUnit.SECONDS)
-    private val paymentRateLimiter = TokenBucketRateLimiter(11, 270, 1, TimeUnit.SECONDS)
+    //case 1:
+    //private val paymentRateLimiter = LeakingBucketRateLimiter(10, java.time.Duration.ofSeconds(1), 10)
+    // case 2:
+    /* private val paymentRateLimiter = CompositeRateLimiter(
+        rl1 = TokenBucketRateLimiter(11, 132, 1, TimeUnit.SECONDS),
+        rl2 = LeakingBucketRateLimiter(10, java.time.Duration.ofSeconds(1), 130)
+    )*/
+    // case 3:
+    private val paymentRateLimiter = CompositeRateLimiter(
+        rl1 = TokenBucketRateLimiter(11, 286, 1, TimeUnit.SECONDS),
+        rl2 = LeakingBucketRateLimiter(10, java.time.Duration.ofSeconds(1), 260)
+    )
 
     @PostMapping("/users")
     fun createUser(@RequestBody req: CreateUserRequest): User {
