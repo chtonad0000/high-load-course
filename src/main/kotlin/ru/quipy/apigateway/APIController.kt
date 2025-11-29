c7b7c7bb38be6da6e3b414cc2c3d87c16a775d92
Sofia <st050447@gmail.com>
2025-11-29 20:30:01 +0300

    lab8

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -25,7 +25,7 @@ class APIController {
     @Autowired
     private lateinit var orderPayer: OrderPayer
 
-    private val paymentRateLimiter = LeakingBucketRateLimiter(10, Duration.ofSeconds(1), 270)
+    private val paymentRateLimiter = LeakingBucketRateLimiter(100, Duration.ofSeconds(1), 270)
 
     @PostMapping("/users")
     fun createUser(@RequestBody req: CreateUserRequest): User {
