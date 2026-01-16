83b17be53fc71fd29d81c1d21cb323a2641a429a
danchub04 <d_chub04@mail.ru>
2026-01-16 18:37:43 +0300

    to 500

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -19,7 +19,7 @@ class OrderPayer(registry: MeterRegistry) {
 
     companion object {
         val logger: Logger = LoggerFactory.getLogger(OrderPayer::class.java)
-        private const val THREAD_COUNT = 300
+        private const val THREAD_COUNT = 500
     }
 
     @Autowired
