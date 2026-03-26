defc28ebcae684389c5394c6f99d6dd92aeb501e
danchub04 <d_chub04@mail.ru>
2026-03-26 22:39:25 +0300

    deadline

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -38,7 +38,7 @@ class PaymentExternalSystemAdapterImpl(
     companion object {
         private val logger = LoggerFactory.getLogger(PaymentExternalSystemAdapter::class.java)
         val mapper: ObjectMapper = ObjectMapper().registerKotlinModule()
-        private const val DEADLINE_BUFFER_MS = 170L
+        private const val DEADLINE_BUFFER_MS = 140L
     }
 
     private val serviceName = properties.serviceName
