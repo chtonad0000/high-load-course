7e8b489ae747a06ab5e705bf63c607e508bf526f
danchub04 <d_chub04@mail.ru>
2026-03-26 22:13:56 +0300

    350 pool

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -61,7 +61,7 @@ class PaymentExternalSystemAdapterImpl(
 
     @OptIn(DelicateCoroutinesApi::class)
     private val paymentScope = CoroutineScope(
-        newFixedThreadPoolContext(500, "payment_pool") + SupervisorJob()
+        newFixedThreadPoolContext(350, "payment_pool") + SupervisorJob()
     )
 
     @OptIn(DelicateCoroutinesApi::class)
